ce4f876f41f395b02355ba01acbabcf0baf3e799
minatwincle <hetaco3710@gmail.com>
2023-02-23 12:40:06 +0900

    条件分岐

--- section6/script.js
+++ section6/script.js
@@ -1,6 +1,6 @@
 let str = document.getElementsByClassName('hello')[0].textContent;
 
-if(!str === 'Hello World!!!') {
+if(str !== 'Hello World!!!') {
   console.log('条件A');
   console.log(str);
 }else if(str === 'Bye World...') {
