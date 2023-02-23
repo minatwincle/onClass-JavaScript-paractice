1dc355a817932bddbcb71b54000f12f2e5a6b02e
minatwincle <hetaco3710@gmail.com>
2023-02-23 10:26:53 +0900

    if文

---
+++ section4/script.js
@@ -0,0 +1,32 @@
+/* var boolean = false;
+if (boolean) {
+ console.log(boolean);
+};
+var num = 100
+if (num == 100) {
+ console.log(num);
+ console.log(num == 100);
+}
+var text = "JavaScript"
+if (text == "JavaScript") {
+ console.log(text);
+}
+ */
+
+// 1. 変数にtrueを代⼊し、ifの条件がtrueの時にconsole.log();で変数の中⾝を表⽰させる処理を作ってください。
+let boo = true;
+if(boo){
+  console.log(boo);
+};
+
+// 2. 変数に100を代⼊し、ifの条件がtrueの時にconsole.log();で変数の中⾝を表⽰させる処理を作ってください。
+let num = 100;
+if(num){
+  console.log(num);
+};
+
+// 3. 変数に"JavaScript"を代⼊し、ifの条件がtrueの時にconsole.log();で変数の中⾝を表⽰させる処理を作ってください。
+let str = 'JavaScript';
+if(str){
+  console.log(str);
+};
